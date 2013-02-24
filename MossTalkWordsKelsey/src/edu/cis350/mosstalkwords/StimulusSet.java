8d37b5a19af1aee243c2cfd37f931ae78d71d232
kelseyds <kelseydds@gmail.com>
2013-02-24 14:50:55 -0500

    added loadData

---
+++ MossTalkWordsKelsey/src/edu/cis350/mosstalkwords/StimulusSet.java
@@ -0,0 +1,28 @@
+package edu.cis350.mosstalkwords;
+
+
+public class StimulusSet {
+	String setName;
+	Stimulus[] stimuli;
+	public StimulusSet(String name, Stimulus[] stimuliInSet)
+	{
+		setName=name;
+		stimuli=stimuliInSet;
+	}
+	public String getName()
+	{
+		return setName;
+	}
+	public Stimulus [] getStimuli()
+	{
+		return stimuli;
+	}
+	public void setName(String name)
+	{
+		setName=name;
+	}
+	public void setStimuli(Stimulus [] stimuliInSet)
+	{
+		stimuli=stimuliInSet;
+	}
+}
