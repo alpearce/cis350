90d507bffe70dfa5a04c621e5a9d336dfc1088f0
kelseyds <kelseydds@gmail.com>
2013-02-24 18:32:37 -0500

    new stimulisets added

--- MossTalkWordsKelsey/src/edu/cis350/mosstalkwords/MainActivity.java
+++ MossTalkWordsKelsey/src/edu/cis350/mosstalkwords/MainActivity.java
@@ -57,7 +57,6 @@ public class MainActivity extends Activity {
 
 		Stimulus livingEasyStimuli[] = new Stimulus [10];
 		Stimulus livingHardStimuli[] = new Stimulus [10];
-		livingHard=new StimulusSet("LivingHard", livingHardStimuli);
 		Stimulus nonlivingEasyStimuli[] = new Stimulus [10];
 		Stimulus nonlivingHardStimuli[] = new Stimulus [10];
 		
@@ -86,6 +85,31 @@ public class MainActivity extends Activity {
 		livingHardStimuli[9] = new Stimulus("Broccoli", 1, null, R.drawable.broccoli);
 		
 		livingHardSet = new StimulusSet("Living Hard", livingHardStimuli);
+		
+		nonlivingEasyStimuli[0] = new Stimulus("Chair", 0, null, R.drawable.chair);
+		nonlivingEasyStimuli[1] = new Stimulus("Table", 0, null, R.drawable.table);
+		nonlivingEasyStimuli[2] = new Stimulus("Lamp", 0, null, R.drawable.lamp);
+		nonlivingEasyStimuli[3] = new Stimulus("Bed", 0, null, R.drawable.bed);
+		nonlivingEasyStimuli[4] = new Stimulus("Phone", 0, null, R.drawable.phone);
+		nonlivingEasyStimuli[5] = new Stimulus("House", 0, null, R.drawable.house);
+		nonlivingEasyStimuli[6] = new Stimulus("Shirt", 0, null, R.drawable.shirt);
+		nonlivingEasyStimuli[7] = new Stimulus("Shoes", 0, null, R.drawable.shoes);
+		nonlivingEasyStimuli[8] = new Stimulus("Hat", 0, null, R.drawable.hat);
+		nonlivingEasyStimuli[9] = new Stimulus("Money", 0, null, R.drawable.money);
+		
+		nonlivingEasySet = new StimulusSet("Nonliving Easy", nonlivingEasyStimuli);
+		
+		nonlivingHardStimuli[0] = new Stimulus("Computer", 1, null, R.drawable.computer);
+		nonlivingHardStimuli[1] = new Stimulus("Textbook", 1, null, R.drawable.textbook);
+		nonlivingHardStimuli[2] = new Stimulus("Television", 1, null, R.drawable.tv);
+		nonlivingHardStimuli[3] = new Stimulus("Refrigerator", 1, null, R.drawable.fridge);
+		nonlivingHardStimuli[4] = new Stimulus("Basketball", 1, null, R.drawable.basketball);
+		nonlivingHardStimuli[5] = new Stimulus("Football", 1, null, R.drawable.football);
+		nonlivingHardStimuli[6] = new Stimulus("Soccerball", 1, null, R.drawable.soccerball);
+		nonlivingHardStimuli[7] = new Stimulus("Pocket", 1, null, R.drawable.pocket);
+		nonlivingHardStimuli[8] = new Stimulus("Zipper", 1, null, R.drawable.zipper);
+		nonlivingHardStimuli[9] = new Stimulus("Gloves", 1, null, R.drawable.gloves);
+		
 		
 
 	}
