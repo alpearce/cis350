6c173649d9a6648f877daf0b5ecf1cc0a6d474c1
Lily Wang <lilyw.416@gmail.com>
2013-03-17 01:27:18 -0400

    trying to pull kelsey's changes

--- MossTalkWordsKelsey/gen/edu/cis350/mosstalkwords/R.java
+++ MossTalkWordsKelsey/gen/edu/cis350/mosstalkwords/R.java
@@ -84,7 +84,8 @@ public final class R {
     }
     public static final class raw {
         public static final int bellringing=0x7f040000;
-        public static final int ding=0x7f040001;
+        public static final int bellringingcorrect=0x7f040001;
+        public static final int ding=0x7f040002;
     }
     public static final class string {
         public static final int app_name=0x7f060000;
