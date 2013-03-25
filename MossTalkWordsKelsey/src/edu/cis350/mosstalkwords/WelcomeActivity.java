ef9c0f52b3b6bd7c68ded2b35c80785f813b2a0d
Lily Wang <lilyw.416@gmail.com>
2013-03-25 18:54:40 -0400

    more welcome page stuff to make the buttons to send to different sets

--- MossTalkWordsKelsey/src/edu/cis350/mosstalkwords/WelcomeActivity.java
+++ MossTalkWordsKelsey/src/edu/cis350/mosstalkwords/WelcomeActivity.java
@@ -1,5 +1,8 @@
 package edu.cis350.mosstalkwords;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,16 +16,19 @@ public class WelcomeActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 		setContentView(R.layout.welcome_page);
-		Button b = (Button)findViewById(R.id.livingeasy);
 		
-		b.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-				finish();
-				
-			}
-		});
+	}
+	public void onClickLivingEasy(View view) {
+		Intent i = getIntent();
+		i.putExtra("indexOfSetsArray", 0);
+		setResult(RESULT_OK, i);
+		finish();
+	}
+	public void onClickLivingMedium(View view) {
 		
+	}
+	public void onClickLivingHard(View view) {
+		 
 	}
 
 }
