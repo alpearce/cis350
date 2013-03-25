4a96781fe954cd788bdbea417faa9f4cb23edfef
kelseyds <kelseydds@gmail.com>
2013-03-25 18:31:09 -0400

    dynamically updated endSetDialog

--- MossTalkWordsKelsey/src/edu/cis350/mosstalkwords/EndSetActivity.java
+++ MossTalkWordsKelsey/src/edu/cis350/mosstalkwords/EndSetActivity.java
@@ -8,6 +8,10 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class EndSetActivity extends Activity {
@@ -16,16 +20,36 @@ public class EndSetActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
+	    //setContentView(R.layout.dialog_endset);
 	    //createDialog();
 	    // TODO Auto-generated method stub
 	    Intent i=getIntent();
 	    currentUser=(User) i.getSerializableExtra("User");
-	
+	    String currentSet= (String) i.getStringExtra("currentSet");
+	    
+		//score.setNumStars(3);
 	    Context mContext = this; //this.getApplicationContext();
-	    LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	   
+	    
+	    
+	    LayoutInflater inflater = this.getLayoutInflater();//(LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	    View layout = inflater.inflate(R.layout.dialog_endset, null);
+	   
+	    RatingBar score=(RatingBar) layout.findViewById(R.id.scoreBar);
+	    int starScore=currentUser.getStarScore(currentSet);
+	    score.setNumStars(starScore);
+	    score.setRating(starScore);
+	    
+	    TextView completeness=(TextView) layout.findViewById(R.id.Completeness);
+	    String efficiencyPercent=new Integer(currentUser.getAverageEfficiencyPercent(currentSet)).toString();
+	    completeness.setText(completeness.getText()+efficiencyPercent+"%");
+	    
+	    TextView streak=(TextView) layout.findViewById(R.id.streak);
+	    String longestStreak=new Integer(currentUser.getLongestStreak(currentSet)).toString();
+	    streak.setText(streak.getText()+longestStreak);
 			AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-			builder.setTitle(R.string.congrats)
-				.setView(inflater.inflate(R.layout.dialog_endset,null))
+			builder.setTitle("           Congratulations!")//hackish extra spaces to center the title since not an option
+				.setView(layout)
 				.setPositiveButton(R.string.restart, new DialogInterface.OnClickListener()
 				{
 					public void onClick(DialogInterface dialog, int id)
@@ -41,8 +65,11 @@ public class EndSetActivity extends Activity {
 					}
 				});
 					
+			
 				AlertDialog alert= builder.create();//create the AlertDialog object and return it
+				//alert.setContentView(R.layout.dialog_endset);
 				alert.show();
+				
 	}
 
 }
--- MossTalkWordsKelsey/src/edu/cis350/mosstalkwords/MainActivity.java
+++ MossTalkWordsKelsey/src/edu/cis350/mosstalkwords/MainActivity.java
@@ -168,6 +168,7 @@ public class MainActivity extends Activity implements ViewFactory {
 	{
 		Intent endSet=new Intent(this, EndSetActivity.class);
 		endSet.putExtra("User", currentUser);
+		endSet.putExtra("currentSet", currentSet.getName());
 		startActivity(endSet);
 	}
 	public void nextImage() {
